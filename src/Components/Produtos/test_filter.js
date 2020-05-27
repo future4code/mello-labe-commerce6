84bfe77b9f6074fc029d5ef494b1c7a5ec6f01ff
Victor Gutierrez <victorgutierrezgomes@gmail.com>
2020-05-26 22:09:35 -0400

    fix1

---
+++ src/Components/Produtos/test_filter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Produtos from './produtos';
+
+const Test = () => {
+    return (
+        <>
+            <Produtos valMin={1} valMax={1000000} />
+        </>
+    );
+};
+
+export default Test;
